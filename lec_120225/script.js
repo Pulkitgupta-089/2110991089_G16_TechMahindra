8676cc9bf9400cdc561b5248605af61f3e888fe2
Pulkitgupta-089 <pulkitgupta934@gmail.com>
2025-02-12 23:34:07 +0530

    functions and its types

---
+++ lec_120225/script.js
@@ -0,0 +1,16 @@
+function abc(){
+    console.log(1);
+}
+
+var b=function xyz(){
+    console.log(2)
+}
+
+var d=()=>{
+    console.log("hello")
+}
+
+// function(){
+//     console.log("adf")
+// }
+
